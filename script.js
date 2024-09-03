d47c1f8a2882269c81cf9ae3b60d1a4cec4d9709
peat20111 <peat20111@gmail.com>
2024-09-03 12:06:23 +0200

    Add files via upload

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach(button => {
-        button.addEventListener("click", function() {
-            alert("Дякуємо за покупку! Ми зв'яжемося з вами найближчим часом.");
-        });
-    });
-});
